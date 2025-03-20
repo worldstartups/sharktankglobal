1e6c9d76d4ae0b116375c4be6bbc5f609d04cea6
worldstartups <ashlesha4475@gmail.com>
2025-03-20 23:13:14 +0530

    indejs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import IndiaPage from "./pages/IndiaPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/india" element={<IndiaPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/india" element={<IndiaPage />} />
+    </Routes>
   );
 }
 
