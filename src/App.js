e3450ed3766a5d865eb838a169f3d37e504c978d
worldstartups <ashlesha4475@gmail.com>
2025-03-21 16:56:28 +0530

    ./

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import InvestorsPage from "./pages/InvestorsPage";
 function App() {
   return (
     <Routes>
-      <Route path="/sharktankglobal" element={<HomePage />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/india" element={<IndiaPage />} />
       <Route path="/india/investors/:season" element={<InvestorsPage />} /> 
     </Routes>
